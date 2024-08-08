8438322a13af39229830ad7aecedb8d13fbe9745
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-08 00:56:15 -0500

    Created getHumanChoice function to return their answer or have them retry if its an invalid input

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 // Create function, getComputerChoice to 
 //return a string value of rock paper or scissors.
-
 //Use math.random to generate a number multiplied by 100
 //Convert to a whole number and divide them out by 3 for each choice.
 function getComputerChoice(){
     let numGenerator = Math.floor(Math.random() * 100);
         if(numGenerator < 33){
-            return "rock";
+            return "Rock";
         } else if(numGenerator < 66){
-            return "scissors";
+            return "Scissors";
         } else{
-            return "paper";
+            return "Paper";
         }
+}
+
+/*Use prompt to get user's choice and run it through
+some if statents to make sure their input is valid.*/
+let userChoice = prompt("Pleaase enter your choice of Rock, Paper, or Scissors: ")
+
+function getHumanChoice(userChoice){
+    if(userChoice === "Rock"){
+        return "Rock";
+    } else if(userChoice === "Paper"){
+        return "Paper";
+    } else if(userChoice === "Scissors"){
+        return "Scissors";
+    } else{
+        alert("You enetered an invalid choice. Please check your spelling and try again.");
+    }
 }
