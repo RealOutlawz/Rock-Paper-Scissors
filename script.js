96bc01592e5cc68e019a62291d815a82b7163a59
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2024-08-16 00:23:53 -0500

    Reworked Rock paper scissors game and moved onto webpage outside of the console

--- script.js
+++ script.js
@@ -1,89 +1,84 @@
-/*Created function named playGame to store all
-functions in */
-function playGame(){
+//Initialize computer and users scores to 0
+let humanScore = 0;
+let computerScore = 0;
 
-    //Initialize computer and users scores to 0
-    let humanScore = 0;
-    let computerScore = 0;
-
-    //Loops through the game 5 times before terminating.
-    for ( let i = 0; i < 5; i++){
-        //create constant variables for the functions and called them
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-
-    /*Create function, getComputerChoice to return a string
-    value of rock paper or scissors. Use math.random to
-    generate a number multiplied by 100 Convert to a 
-    hole number and divide them out by 3 for each choice. */
-    function getComputerChoice(){
-        let numGenerator = Math.floor(Math.random() * 100);
-            if(numGenerator < 33){
-                console.log("The computer picked Rock");
-                return "Rock";
-            } else if(numGenerator < 66){
-                console.log("The computer picked Scissors");
-                return "Scissors";
-            } else{
-                console.log("The computer picked Paper");
-                return "Paper";
-            }
-    }
-    
-    /*Use prompt to get user's choice and run it through
-    some if statents to make sure their input is valid.*/
-    function getHumanChoice(){
-        let userChoice = prompt("Please enter your choice of Rock, Paper, or Scissors: ")
-        userChoice = userChoice.toLowerCase(); 
-        if(userChoice === "rock"){
-            console.log("You picked Rock");
-            return "Rock";
-        } else if(userChoice === "paper"){
-            console.log("You picked Paper");
-            return "Paper";
-        } else if(userChoice === "scissors"){
-            console.log("You picked Scissors");
-            return "Scissors";
-        } else{
-            console.log("You entered an invalid choice. Please check your spelling and try again.");
-            getHumanChoice();
-        }
-    }
-
-
-    /* Get user and computer choices, and put them as argument
-    against each other to determine the winnner and update the
-    scores.
-    */
-    function playRound(humanChoice, computerChoice){
-        if(humanChoice === "Rock" && computerChoice === "Paper" ||
-        humanChoice === "Paper" && computerChoice === "Scissors" ||
-        humanChoice === "Scissors" && computerChoice === "Rock"){
-            console.log("The computer won this round!");
-            computerScore++;
-        } else if(humanChoice === "Rock" && computerChoice === "Scissors" ||
-                humanChoice === "Paper" && computerChoice === "Rock" ||
-                humanChoice === "Scissors" && computerChoice === "Paper"){
-                    console.log("You won this round!");
-                    humanScore++;
-        } else if(humanChoice === computerChoice){
-            console.log("Tied! No winners, try again!")
-        }else if(humanChoice === null){
-            return 0;
-        } 
-    }
-
-    console.log("Your Score: " + humanScore);
-    console.log("Computer Score: " + computerScore); 
-
-    if(humanScore > computerScore){
-    console.log("You won!")
-    } else if(humanScore < computerScore){
-    console.log("You lost!")
-    } else {
-    console.log("You and the computer tied!")
-    }
+/*get the computers choice, use math.random to
+    generate a number multiplied by 100 convert to a 
+    whole number and divide them out by 3 for each choice. */
+function getComputerChoice() {
+  let numGenerator = Math.floor(Math.random() * 100);
+  if (numGenerator < 33) {
+    return "Rock";
+  } else if (numGenerator < 66) {
+    return "Scissors";
+  } else {
+    return "Paper";
+  }
 }
-playGame();
+
+/* Get user choice and put them as argument to determine the winnner 
+and update the scores. */
+function playRound(humanChoice) {
+  const computerChoice = getComputerChoice();
+  document.getElementById(
+    "compSelection"
+  ).textContent = `The computer picked ${computerChoice}`;
+  document.getElementById(
+    "userSelection"
+  ).textContent = `You picked ${humanChoice}`;
+
+  let roundWinner = "";
+
+  if (
+    (humanChoice === "Rock" && computerChoice === "Paper") ||
+    (humanChoice === "Paper" && computerChoice === "Scissors") ||
+    (humanChoice === "Scissors" && computerChoice === "Rock")
+  ) {
+    roundWinner = "The computer won this round!";
+    computerScore++;
+  } else if (
+    (humanChoice === "Rock" && computerChoice === "Scissors") ||
+    (humanChoice === "Paper" && computerChoice === "Rock") ||
+    (humanChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    roundWinner = "You won this round!";
+    humanScore++;
+  } else {
+    roundWinner = "Tied! No winners, try again!";
+  }
+
+  let yourScore = document.getElementById("yourScore");
+  let compScore = document.getElementById("compScore");
+
+  document.getElementById("roundWinner").textContent = roundWinner;
+  yourScore.textContent = `Your Score: ${humanScore}`;
+  compScore.textContent = `Computer Score: ${computerScore}`;
+
+  if (humanScore >= 5 || computerScore >= 5) {
+    checkWinner();
+  }
+}
+
+function checkWinner() {
+  let overallWinner = "";
+
+  if (humanScore > computerScore) {
+    overallWinner = "You won!";
+  } else if (humanScore < computerScore) {
+    overallWinner = "You lost!";
+  } else {
+    overallWinner = "You and the computer tied!";
+  }
+
+  document.getElementById("overallWinner").textContent = overallWinner;
+}
+
+document.getElementById("rock").addEventListener("click", () => {
+  playRound("Rock");
+});
+document.getElementById("paper").addEventListener("click", () => {
+  playRound("Paper");
+});
+document.getElementById("scissors").addEventListener("click", () => {
+  playRound("Scissors");
+});
